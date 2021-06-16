08af9330581dc6adccaf74e64a11d251310aff71
Erick cuenca <4173498@alu.murciaeduca.es>
2021-06-16 09:43:56 +0200

    commit del ejerciicio 13

---
+++ src/practicataller/Ejmplo_Erick.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicataller;
+
+/**
+ *
+ * @author Erick cuenca <4173498@alu.murciaeduca.es>
+ */
+public class Ejmplo_Erick {
+    /**
+     * Esto es un ejemplo del fork del ejercicio 13
+     */
+}
